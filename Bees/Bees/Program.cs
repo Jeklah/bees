3154422c061bd2c5567dcbca76197098de212502
Jeklah <arthurbowers@gmail.com>
2020-07-23 11:07:14 +0100

    Added dead flag and driver class.

--- Bees/Bees/Program.cs
+++ Bees/Bees/Program.cs
@@ -1,20 +1,28 @@
 ﻿using System;
 
-namespace _Bees { // Added underscore to start of namespace to avoid confusion with class name.
-    public class Bees {
-        protected float health = 100;
-        protected int DEATH;  // capitals as this will be a constant for each child class, albeit different for each child class.
+namespace _Bees
+{ // Added underscore to start of namespace to avoid confusion with class name.
+    public class Bees
+    {
+        private float health = 100;
+        public int DEATH;  // capitals as this will be a constant for each child class, albeit different for each child class.
+        public Boolean dead = false;
 
-        public void damage(int dmg) {
-            if (0 <= dmg && dmg <= 100) {  // Input check to see if it is within the correct range.
+
+        public void damage(int dmg)
+        {
+            if (0 <= dmg && dmg <= 100)
+            {  // Input check to see if it is within the correct range.
                 health = health - dmg;
             }
-            else {
+            else
+            {
                 Console.WriteLine("Please enter a valid number between 0 and 100.");
             }
         }
 
-        public string getHealth() {
+        public string getHealth()
+        {
             return this.health.ToString();
         }
 
@@ -24,27 +32,38 @@ namespace _Bees { // Added underscore to start of namespace to avoid confusion w
         }
     }
 
-    public class Worker: Bees {
+    public class Worker : Bees
+    {
 
-        public Worker() {  // constructor for Worker class.
+        public Worker()
+        {  // constructor for Worker class.
             this.DEATH = 70;
         }
     }
 
-    public class Drone: Bees {
-        public Drone() {
+    public class Drone : Bees
+    {
+        public Drone()
+        {  // constructor for Drone class
             this.DEATH = 50;
         }
     }
 
-    public class Queen: Bees {
-        public Queen() {
+    public class Queen : Bees
+    { // constructor for Queen class
+        public Queen()
+        {
             this.DEATH = 20;
         }
     }
 
-    static void Main(string[] args) {
-        Console.WriteLine("Hello World!");
+    class Driver
+    { // driver class
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
     }
 }
 
